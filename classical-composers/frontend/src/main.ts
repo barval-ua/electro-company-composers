74ead1d7da506c8debe87dd653ba456e664ebc37
Andrii Bochkov <bochkov.dev@gmail.com>
2024-10-25 15:38:27 +0300

    [frontend] Add application-level error handler

--- classical-composers/frontend/src/main.ts
+++ classical-composers/frontend/src/main.ts
@@ -5,10 +5,18 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { HttpError } from './api/HttpError'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.config.errorHandler = (err, instance, info) => {
+    console.error(err, instance, info); // log elsewhere in production
+    if (err instanceof HttpError) { // generally - check if Error is appropriate for user's eyes
+        alert(err.message);
+    }
+};
 
 app.mount('#app')
